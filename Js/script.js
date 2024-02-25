374b42f1f8aea6fe2e71c15488879fd13558b4c2
Ricky <rickythoudam123@gmail.com>
2024-02-25 23:28:42 +0530

    small toggle config

--- Js/script.js
+++ Js/script.js
@@ -1,14 +1,27 @@
-const btn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
+// const btn = document.getElementById('menu-btn');
+// const menu = document.getElementById('menu');
 
-btn.addEventListener('click', navToggle);
+// btn.addEventListener('click', navToggle);
 
 
 
+
+// function navToggle() {
+//     btn.classList.toggle('open');
+//     menu.classList.toggle('flex'); 
+//     menu.classList.toggle('hidden'); 
+// }
+
+
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+btn.addEventListener('click', navToggle)
 
 function navToggle() {
-    btn.classList.toggle('open');
-    menu.classList.toggle('flex'); 
-    menu.classList.toggle('hidden'); 
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
 }
+
 
